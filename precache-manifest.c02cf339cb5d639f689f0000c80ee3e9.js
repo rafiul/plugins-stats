95ea37670f3bd1cafb0f01414b872b539c6aa455
rafiul <bmrafiul.alam@gmail.com>
2020-05-29 06:19:12 +0600

    Updates

---
+++ precache-manifest.c02cf339cb5d639f689f0000c80ee3e9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86a5aef8df15cccc5668d6e5707017b2",
+    "url": "/plugins-stats/index.html"
+  },
+  {
+    "revision": "71094eccd2c9cd0c902c",
+    "url": "/plugins-stats/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "3f1c5a7941ba049c8f88",
+    "url": "/plugins-stats/static/css/main.b1d1776e.chunk.css"
+  },
+  {
+    "revision": "71094eccd2c9cd0c902c",
+    "url": "/plugins-stats/static/js/2.e5d69946.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/plugins-stats/static/js/2.e5d69946.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f1c5a7941ba049c8f88",
+    "url": "/plugins-stats/static/js/main.3ae0966a.chunk.js"
+  },
+  {
+    "revision": "6b5139c39d23fcd1d73a",
+    "url": "/plugins-stats/static/js/runtime-main.33a49b02.js"
+  },
+  {
+    "revision": "374254562088d383930a1616d152cd83",
+    "url": "/plugins-stats/static/media/loading.37425456.svg"
+  },
+  {
+    "revision": "650c323eeaaa445664b283305a5e4419",
+    "url": "/plugins-stats/static/media/logo.650c323e.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.25eb45c5edbe7de169c07833074c1359.js"
+  "/plugins-stats/precache-manifest.c02cf339cb5d639f689f0000c80ee3e9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/plugins-stats/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
