3dc13c328c622a7660d96006f218d9f4c1e89b10
rafiul <bmrafiul.alam@gmail.com>
2021-04-09 20:11:55 +0600

    Updates

--- precache-manifest.3288dadc4c5813f4712a1d8617a6c013.js
+++ precache-manifest.3288dadc4c5813f4712a1d8617a6c013.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5a110d5abecd916e3fd537f5423568c5",
+    "revision": "a99ed4aaa77eeb41f98071c319dae402",
     "url": "/plugins-stats/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/plugins-stats/static/css/2.829c9cb5.chunk.css"
   },
   {
-    "revision": "d86a1eb28f2c9a9dde54",
+    "revision": "237551bcca456cf73e24",
     "url": "/plugins-stats/static/css/main.3672b7ad.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/plugins-stats/static/js/2.784c0a10.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "d86a1eb28f2c9a9dde54",
-    "url": "/plugins-stats/static/js/main.138fdae4.chunk.js"
+    "revision": "237551bcca456cf73e24",
+    "url": "/plugins-stats/static/js/main.54843191.chunk.js"
   },
   {
     "revision": "6b5139c39d23fcd1d73a",
